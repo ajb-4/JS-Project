d7e0215e6d54c8ef15d937f719f09300f2865c56
ajb-4 <alexbrown4711@gmail.com>
2023-08-29 12:18:06 -0400

    id's and classes refactored, need to style them

--- src/scripts/scenario.js
+++ src/scripts/scenario.js
@@ -44,20 +44,20 @@ scenarios.innerHTML +=  `<div class="scenario" id="scenario${this.num}">
     <input type="text" name="invVehicle" id="invVehicle${this.num}">
   </label>
   <br>
-  <input type="button" value="Show Yield" class="testbuttonyield${this.num}">
+  <input type="button" value="Show Yield" class="testbuttonyield" id="testbuttonyield${this.num}">
   <br>
   <div id="apifetcher${this.num}">Total Yield</div>
 </div>
 
 
 <div class="yieldbox">
-  <input type="button" value="Customize My Basket" class="customizebasket${this.num}">
+  <input type="button" value="Customize My Basket" class="customizebasket" id="customizebasket${this.num}">
   <br>
-  <input type="button" value="Show Me The Goods!" class="testbuttongoods${this.num}">
+  <input type="button" value="Show Me The Goods!" class="testbuttongoods" id="testbuttongoods${this.num}">
   <br>
   <div id="apifetch${this.num}">Goods Hash</div>
 </div>
-<input type="button" value="Delete Scenario ${this.num}" class="deleteScenario${this.num}">
+<input type="button" value="Delete Scenario ${this.num}" class="deleteScenario" id="deleteScenario${this.num}">
 </div>
 
         <div class="modalcontainer" id="modalcontainer${this.num}">
@@ -75,12 +75,11 @@ scenarios.innerHTML +=  `<div class="scenario" id="scenario${this.num}">
             <label for="">Coffee:
               <input type="number" name="coffeemodal" id="coffeemodal${this.num}">
             </label>
-            <input type="button" value="X" id="exitmodal${this.num}">
-            <input type="button" value="Save Basket" id="savemodal${this.num}">
+            <input type="button" value="Close Basket" id="exitmodal${this.num}">
           </div>
-        </div> `
+        </div>`
 
-    }
+    };
     
     deleteScenario(scen) {
       debugger
@@ -97,7 +96,7 @@ scenarios.innerHTML +=  `<div class="scenario" id="scenario${this.num}">
       let duration
       let invVehicle
 
-      const testButtonYield = document.querySelector(`.testbuttonyield${this.num}`);
+      const testButtonYield = document.getElementById(`testbuttonyield${this.num}`);
 
       testButtonYield.addEventListener("click", () => {
         invAmount = parseFloat(document.getElementById(`invAmount${this.num}`).value);
@@ -117,7 +116,7 @@ scenarios.innerHTML +=  `<div class="scenario" id="scenario${this.num}">
       let sugarPref
       let coffPref
 
-      const testButtonGoods = document.querySelector(`.testbuttongoods${this.num}`);
+      const testButtonGoods = document.getElementById(`testbuttongoods${this.num}`);
 
     testButtonGoods.addEventListener("click", () => {
       totalYield = document.getElementById(`apifetcher${this.num}`).getAttribute("value");
@@ -131,7 +130,7 @@ scenarios.innerHTML +=  `<div class="scenario" id="scenario${this.num}">
       determineQuantities(totalYield, endDate, preferences, scengood);
     });
 
-    const deleteScenarioButton = document.querySelector(`.deleteScenario${this.num}`);
+    const deleteScenarioButton = document.getElementById(`deleteScenario${this.num}`);
 
     deleteScenarioButton.addEventListener("click", () => {
       debugger
@@ -139,7 +138,7 @@ scenarios.innerHTML +=  `<div class="scenario" id="scenario${this.num}">
       this.deleteScenario(scenDelete);
     });
 
-    const custBasketButton = document.querySelector(`.customizebasket${this.num}`);
+    const custBasketButton = document.getElementById(`customizebasket${this.num}`);
 
     custBasketButton.addEventListener("click", () => {
         const modalcontainer = document.getElementById(`modalcontainer${this.num}`);
@@ -154,7 +153,7 @@ scenarios.innerHTML +=  `<div class="scenario" id="scenario${this.num}">
         modalcontainer.style.display = "none";
     });
   
-    }
+  }
 
     
 };
